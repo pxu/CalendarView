19c5881cf5748c347d1dcb32d4134f4a19c0d662
PENG FEI XU <pengfei.xu@aeroinfo.com>
2017-11-20 22:26:29 -0800

    added unittest for medicineTrackermanager

---
+++ app/src/androidTest/java/com/douglascollege/a300216962/medicinetracker/MedicineTrackerManagerTest.java
@@ -0,0 +1,69 @@
+package com.douglascollege.a300216962.medicinetracker;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pengfei.xu on 11/20/2017.
+ */
+public class MedicineTrackerManagerTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void createMedicineTracker() throws Exception {
+    }
+
+    @org.junit.Test
+    public void createMedicineTrackerItem() throws Exception {
+    }
+
+    @org.junit.Test
+    public void createMedicineTrackerItemInCloud() throws Exception {
+    }
+
+    @org.junit.Test
+    public void remindMedicineTrackerItemInCloud() throws Exception {
+    }
+
+    @org.junit.Test
+    public void removeMedicineTrackerItemReminderInCloud() throws Exception {
+    }
+
+    @org.junit.Test
+    public void removeMedicineTrackerItemInCloud() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getMedicineTrackerById() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getAllMedicineTrackers() throws Exception {
+    }
+
+    @org.junit.Test
+    public void hasMedicineAlreadyTracked() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isMedicineTrackerCompletedSuccessfully() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getMedicineTrackerProgress() throws Exception {
+    }
+
+    @org.junit.Test
+    public void deleteMedicineTracker() throws Exception {
+    }
+
+    @org.junit.Test
+    public void updateMedicineTrackerItem() throws Exception {
+    }
+
+}
